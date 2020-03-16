407a9d27f7d385c3a98a71443e4c3d243dd04430
Kristoffer Alquiza <kalquiza@gmail.com>
2020-03-15 19:26:45 -0700

    Setup node evironment with express

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
+// Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
+/* Dependencies */
+const bodyParser = require('body-parser')
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Spin up the server
+const port = 8000;
+const server = app.listen(port, listening);
+function listening() {
+    // console.log(server);
+    console.log(`running on localhost: ${port}`);
+};
 
-// Setup Server
+// Callback to debug
+
+// Initialize all route with a callback function
+// Callback function to complete GET '/all'
+app.get('/all', (req, res) => {
+    res.send('GET request to the homepage')
+})
+// Post Route
+app.post('/', (req, res) => {
+    res.send('POST request to the homepage')
+})
