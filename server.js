85987b3457ef03b2e615ddf5f32f4baf31f73484
Kristoffer Alquiza <kalquiza@gmail.com>
2020-03-17 21:06:59 -0700

    Update project data to use JS object
    Additional formatting changes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-projectData = [];
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+let entries = 0;
 
 // Express to run server and routes
 const express = require('express');
@@ -7,10 +9,10 @@ const express = require('express');
 const app = express();
 
 /* Dependencies */
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+// Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 // Cors for cross origin allowance
 const cors = require('cors');
@@ -21,25 +23,29 @@ app.use(express.static('website'));
 
 // Spin up the server
 const port = 3000;
+// eslint-disable-next-line no-unused-vars
 const server = app.listen(port, listening);
+// eslint-disable-next-line require-jsdoc
 function listening() {
-    // console.log(server);
-    console.log(`running on localhost: ${port}`);
+  // console.log(server);
+  console.log(`running on localhost: ${port}`);
 };
 
 // GET Route
 app.get('/all', (req, res) => {
-    res.send(projectData);
-})
+  res.send(projectData);
+});
 
 // POST Route
 app.post('/', (req, res) => {
-    newEntry = {
-        temperature: req.body.temperature,
-        date: req.body.date,
-        feelings: req.body.feelings
-      }
-    
-      projectData.push(newEntry)
-    }
-)
+  newEntry = {
+    temperature: req.body.temperature,
+    date: req.body.date,
+    feelings: req.body.feelings,
+  };
+
+  projectData[entries] = newEntry;
+  entries++;
+  console.log(projectData);
+},
+);
--- website/app.js
+++ website/app.js
@@ -1,79 +1,83 @@
-/* Global Variables */
-
 /* Function to POST data to application */
 const postData = async (url = '', data = {}) => {
-    console.log(data)
-    const response = await fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data), // body data type must match "Content-Type" header        
-    });
+  console.log(data);
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    // body data type must match "Content-Type" header
+    body: JSON.stringify(data),
+  });
 
-    try {
-        const newData = await response.json();
-        return newData
-    } catch (error) {
-        console.log("error", error);
-    }
-}
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
 
 /* Function to GET data from web API OpenWeatherMap */
-let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
-let apiKey = '&appid=5797794e603dad693680b2137143003c';
-let units = '&units=imperial'
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=5797794e603dad693680b2137143003c';
+const units = '&units=imperial';
 
 const getWeather = async (baseURL, zip, key) => {
-
-    const res = await fetch(baseURL + zip + units + key)
-    try {
-        const data = await res.json();
-        return data;
-    } catch (error) {
-        console.log("error", error);
-    }
-}
+  const res = await fetch(baseURL + zip + units + key);
+  try {
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
 
 /* Add event listener to generate new entry */
 document.getElementById('generate').addEventListener('click', performAction);
 
-function performAction(e){
-  const zip =  document.getElementById('zip').value;
+// eslint-disable-next-line require-jsdoc
+function performAction(e) {
+  const zip = document.getElementById('zip').value;
   getWeather(baseURL, zip, apiKey)
-  .then(function(data){
+      .then((data) => {
+        /* Build weather journal entry */
 
-    /* Build weather journal entry */
+        // Get the current date
+        // Create a new date instance dynamically with JS
+        const d = new Date();
+        const newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-    // Get the current date
-    // Create a new date instance dynamically with JS
-    let d = new Date();
-    let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+        // Get the temperature
+        const currentTemp = data.main.temp;
 
-    // Get the temperature
-    let currentTemp = data.main.temp;
+        // Get the user input
+        const feelings = document.getElementById('feelings').value;
 
-    // Get the user input
-    let feelings =  document.getElementById('feelings').value;
-
-    // Post data to app
-    postData('/', {temperature: currentTemp, date: newDate, feelings: feelings} )
-    .then(
-        updateUI()
-    )
-  })
+        // Post data to app
+        postData('/', {
+          temperature: currentTemp,
+          date: newDate,
+          feelings: feelings,
+        }).then(
+            updateUI(),
+            (error) => {
+              console.log('error', error);
+            });
+      }, (error) => {
+        console.log('error', error);
+      });
 }
 
 const updateUI = async () => {
   const request = await fetch('/all');
-  try{
+  try {
     const allData = await request.json();
-    document.getElementById('date').innerHTML = allData[allData.length - 1].date;
-    document.getElementById('temp').innerHTML = allData[allData.length - 1].temperature;
-    document.getElementById('content').innerHTML = allData[allData.length - 1].feelings;
-
-  }catch(error){
-    console.log("error", error);
+    document.getElementById('date').innerHTML = allData[Object.keys(allData).length - 1].date;
+    document.getElementById('temp').innerHTML = allData[Object.keys(allData).length - 1].temperature;
+    document.getElementById('content').innerHTML = allData[Object.keys(allData).length - 1].feelings;
+  } catch (error) {
+    console.log('error', error);
   }
-}
+};
