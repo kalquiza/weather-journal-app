8b74483f1cd830d8d4baef0f5f81902c101374ad
Kristoffer Alquiza <kalquiza@gmail.com>
2020-03-17 17:58:10 -0700

    Add async function to post data

--- website/app.js
+++ website/app.js
@@ -1,8 +1,28 @@
 /* Global Variables */
 
-/* Function to POST data to application TODO*/
+/* Function to POST data to application */
+const postData = async (url = '', data = {}) => {
+    console.log(data)
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
 
-/* Function to GET data from web API OpenWeatherMap*/
+    try {
+        const newData = await response.json();
+        // console.log(newData);
+        return newData
+    } catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+}
+
+/* Function to GET data from web API OpenWeatherMap */
 let baseURL = 'api.openweathermap.org/data/2.5/weather?zip='
 let apiKey = '&appid=5797794e603dad693680b2137143003c';
 
